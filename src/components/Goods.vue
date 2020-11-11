e158249b82b6deb2290afbfe067fa52e82407bc6
unknown <mashagi@yandex.ru>
2020-11-11 23:16:15 +0300

    first commit

---
+++ src/components/Goods.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="goods"> 
+		<div class="wrapper wrapper--violet">
+			<h1 class="goods__heading">igooods — это <span class="goods__nowrap">80 000</span> товаров<br> <span class="goods__nowrap">и <span class="goods__yellow"> 150 гипермаркетов</span> по всей России </span></h1>
+			<div class="goods__row">
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/fruit.png">
+					<p class="goods__text goods__text--small">Овощи и фрукты</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/children.png">
+					<p class="goods__text goods__text--small">Детские товары</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/household.png">
+					<p class="goods__text goods__text--small">Бытовая химия</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/milk.png">
+					<p class="goods__text goods__text--small">Молочные продукты</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/bread.png">
+					<p class="goods__text goods__text--small">Хлеб и выпечка</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/semifinished.png">
+					<p class="goods__text goods__text--small">Полуфабрикаты</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/fish.png">
+					<p class="goods__text goods__text--small">Рыба и морепродукты</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/meat.png">
+					<p class="goods__text goods__text--small">Мясо и птица</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/tea.png">
+					<p class="goods__text goods__text--small">Чай, кофе, какао</p>
+				</a>
+				<a href="#" class="goods__link goods__link--card">
+					<img class="goods__img goods__img--violet" src="img/categories/desert.png">
+					<p class="goods__text goods__text--small">Сладкое</p>
+				</a>
+			</div>
+			<a href="#" class="goods__link goods__link--button link--button">Выбрать магазин и посмотреть каталог</a>
+		</div>
+	</section>
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,86 @@
+<template>
+  <header class="header">
+		<section class="header__top">
+			<div class="wrapper wrapper--header">
+				<img class="header__img" :src="require('@/assets/img/label.jpg')" alt="логотип">
+				<div class="header__list">
+					<a href="#" class="link link--top link--disappear">Санкт-Петербург и ЛО</a>
+					<a href="#" class="link link--top link--disappear">Список магазинов</a>
+					<a href="#" class="link link--top link--disappear">Как это работает</a>
+					<a href="#" class="link link--top link--disappear">Доставка для бизнеса</a>
+					<a href="#" class="link link--top link--disappear">Войти</a>
+					<a href="#" class="link link--top link--phone"><i class="fa fa-phone header__icon"></i></a>
+					<a href="#" class="link link--button link--small link--disappear">Заказать доставку</a>
+				</div>
+				<button class="hamburger" type="menu"></button>
+			</div>
+		</section>
+		<section class="header__container">
+			<section class="wrapper wrapper--profits">
+				<div class="profits__carousel owl-carousel">
+					<div class="profits__wrap profits__wrap--home">
+						<div class="profits__discription">
+							<h2 class="profits__title">доставка<br>продуктов<br>на дом</h2>
+							<p class="profits__text">Продукты из самых популярных<br>гипермаркетов Санкт-Петербурга</p>
+							<div class="profits__link-wrap">
+								<a href="#" class="link link--button">Заказать доставку</a>
+								<a href="#" class="link link--violet">Выбрать магазин</a>
+							</div>
+						</div>
+						<img class="profits__img profits__img--delivery" src="img/bgi-delivery.png" alt="картинка акционные товары Ленты">
+					</div>
+					<div class="profits__wrap profits__wrap--catalog" style="background-image: url(img/bgi-catalog.png)">
+						<div class="profits__discription">
+							<h2 class="profits__title">самый<br>актуальный<br>каталог</h2>
+							<p class="profits__text">Ежедневно мы выгружаем актуальный каталог<br>товаров для каждого магазина</p>
+							<div class="profits__link-wrap">
+								<a href="#" class="link link--button">Смотреть каталог</a>
+								<a href="#" class="link link--violet">Выбрать магазин</a>
+							</div>
+						</div>
+					</div>
+					<div class="profits__wrap profits__wrap--pack" style="background-image: url(img/bgi-pack.png)">
+						<div class="profits__discription">
+							<h2 class="profits__title profits__title--navy">Фасуем товар</h2>
+							<p class="profits__text profits__text--navy">Отдельно овощи, фрукты, хозтовары<br>и заморозка. Соблюдаем<br>температурный режим</p>
+							<div class="profits__link-wrap">
+								<a href="#" class="link link--button">Как это работает</a>
+								<a href="#" class="link link--violet">Заказать доставку</a>
+							</div>
+						</div>
+					</div>
+					<div class="profits__wrap profits__wrap--app" >
+						<div class="profits__discription">
+							<h2 class="profits__title profits__title--navy">Пиложение<br>IOS и Android</h2>
+							<p class="profits__text profits__text--navy">Скачайте приложение и заказывайте в любое<br>время и в любом месте</p>
+							<div class="profits__link-wrap">
+								<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/appstore.svg"></a>
+								<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/google.svg"></a>
+							</div>
+						</div>
+						<img class="profits__img profits__img--phone" :src="require('@/assets/img/bg-app.png')" alt="картинка акционные товары Ленты">
+					</div>
+					<div class="profits__wrap profits__wrap--office" style="background-image: url(img/bgi-office.png)">
+						<div class="profits__discription">
+							<h2 class="profits__title profits__title--navy">Доставляем даже<br>в офисы</h2>
+							<p class="profits__text profits__text--navy">Можем регулярно привозить кофе, чай,<br>	молоко, овощи и фрукты в офис.</p>
+							<div class="profits__link-wrap">
+								<a href="#" class="link link--button link--wide">Подробнее для бизнеса</a>
+								<a href="#" class="link link--violet">Выбрать магазин</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="wrapper wrapper--shops">
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-metro.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-lenta.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-prisma.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-babylon.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-vkusvill.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-karusel.svg"></a>
+				<a href="#" class="shops__links"><img class="shops__img" src="img/label-shop/label-auchan.svg"></a>
+			</section>
+		</section>
+	</header>
+</template>
