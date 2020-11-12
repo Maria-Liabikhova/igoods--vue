810e002747275be6097f31e3bf73bedb89488b07
unknown <mashagi@yandex.ru>
2020-11-12 14:33:38 +0300

    add: The VueJS wrapper for Owl Carousel

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -18,60 +18,62 @@
 		</section>
 		<section class="header__container">
 			<section class="wrapper wrapper--profits">
-				<div class="profits__carousel owl-carousel">
-					<div class="profits__wrap profits__wrap--home">
-						<div class="profits__discription">
-							<h2 class="profits__title">доставка<br>продуктов<br>на дом</h2>
-							<p class="profits__text">Продукты из самых популярных<br>гипермаркетов Санкт-Петербурга</p>
-							<div class="profits__link-wrap">
-								<a href="#" class="link link--button">Заказать доставку</a>
-								<a href="#" class="link link--violet">Выбрать магазин</a>
+				<carousel>
+					<div class="profits__carousel owl-carousel">
+						<div class="profits__wrap profits__wrap--home">
+							<div class="profits__discription">
+								<h2 class="profits__title">доставка<br>продуктов<br>на дом</h2>
+								<p class="profits__text">Продукты из самых популярных<br>гипермаркетов Санкт-Петербурга</p>
+								<div class="profits__link-wrap">
+									<a href="#" class="link link--button">Заказать доставку</a>
+									<a href="#" class="link link--violet">Выбрать магазин</a>
+								</div>
+							</div>
+							<img class="profits__img profits__img--delivery" :src="require('@/assets/img/bgi-delivery.png')"  alt="картинка акционные товары Ленты">
+						</div>
+						<div class="profits__wrap profits__wrap--catalog" style="background-image: url(img/bgi-catalog.png)">
+							<div class="profits__discription">
+								<h2 class="profits__title">самый<br>актуальный<br>каталог</h2>
+								<p class="profits__text">Ежедневно мы выгружаем актуальный каталог<br>товаров для каждого магазина</p>
+								<div class="profits__link-wrap">
+									<a href="#" class="link link--button">Смотреть каталог</a>
+									<a href="#" class="link link--violet">Выбрать магазин</a>
+								</div>
 							</div>
 						</div>
-						<img class="profits__img profits__img--delivery" :src="require('@/assets/img/bgi-delivery.png')"  alt="картинка акционные товары Ленты">
-					</div>
-					<div class="profits__wrap profits__wrap--catalog" style="background-image: url(img/bgi-catalog.png)">
-						<div class="profits__discription">
-							<h2 class="profits__title">самый<br>актуальный<br>каталог</h2>
-							<p class="profits__text">Ежедневно мы выгружаем актуальный каталог<br>товаров для каждого магазина</p>
-							<div class="profits__link-wrap">
-								<a href="#" class="link link--button">Смотреть каталог</a>
-								<a href="#" class="link link--violet">Выбрать магазин</a>
+						<div class="profits__wrap profits__wrap--pack" style="background-image: url(img/bgi-pack.png)">
+							<div class="profits__discription">
+								<h2 class="profits__title profits__title--navy">Фасуем товар</h2>
+								<p class="profits__text profits__text--navy">Отдельно овощи, фрукты, хозтовары<br>и заморозка. Соблюдаем<br>температурный режим</p>
+								<div class="profits__link-wrap">
+									<a href="#" class="link link--button">Как это работает</a>
+									<a href="#" class="link link--violet">Заказать доставку</a>
+								</div>
+							</div>
+						</div>
+						<div class="profits__wrap profits__wrap--app" >
+							<div class="profits__discription">
+								<h2 class="profits__title profits__title--navy">Пиложение<br>IOS и Android</h2>
+								<p class="profits__text profits__text--navy">Скачайте приложение и заказывайте в любое<br>время и в любом месте</p>
+								<div class="profits__link-wrap">
+									<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/appstore.svg"></a>
+									<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/google.svg"></a>
+								</div>
+							</div>
+							<img class="profits__img profits__img--phone" :src="require('@/assets/img/bg-app.png')" alt="картинка акционные товары Ленты">
+						</div>
+						<div class="profits__wrap profits__wrap--office" style="background-image: url(img/bgi-office.png)">
+							<div class="profits__discription">
+								<h2 class="profits__title profits__title--navy">Доставляем даже<br>в офисы</h2>
+								<p class="profits__text profits__text--navy">Можем регулярно привозить кофе, чай,<br>	молоко, овощи и фрукты в офис.</p>
+								<div class="profits__link-wrap">
+									<a href="#" class="link link--button link--wide">Подробнее для бизнеса</a>
+									<a href="#" class="link link--violet">Выбрать магазин</a>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="profits__wrap profits__wrap--pack" style="background-image: url(img/bgi-pack.png)">
-						<div class="profits__discription">
-							<h2 class="profits__title profits__title--navy">Фасуем товар</h2>
-							<p class="profits__text profits__text--navy">Отдельно овощи, фрукты, хозтовары<br>и заморозка. Соблюдаем<br>температурный режим</p>
-							<div class="profits__link-wrap">
-								<a href="#" class="link link--button">Как это работает</a>
-								<a href="#" class="link link--violet">Заказать доставку</a>
-							</div>
-						</div>
-					</div>
-					<div class="profits__wrap profits__wrap--app" >
-						<div class="profits__discription">
-							<h2 class="profits__title profits__title--navy">Пиложение<br>IOS и Android</h2>
-							<p class="profits__text profits__text--navy">Скачайте приложение и заказывайте в любое<br>время и в любом месте</p>
-							<div class="profits__link-wrap">
-								<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/appstore.svg"></a>
-								<a href="#" class="link link--violet"><img class="profits__img profits__img--soc" src="img/google.svg"></a>
-							</div>
-						</div>
-						<img class="profits__img profits__img--phone" :src="require('@/assets/img/bg-app.png')" alt="картинка акционные товары Ленты">
-					</div>
-					<div class="profits__wrap profits__wrap--office" style="background-image: url(img/bgi-office.png)">
-						<div class="profits__discription">
-							<h2 class="profits__title profits__title--navy">Доставляем даже<br>в офисы</h2>
-							<p class="profits__text profits__text--navy">Можем регулярно привозить кофе, чай,<br>	молоко, овощи и фрукты в офис.</p>
-							<div class="profits__link-wrap">
-								<a href="#" class="link link--button link--wide">Подробнее для бизнеса</a>
-								<a href="#" class="link link--violet">Выбрать магазин</a>
-							</div>
-						</div>
-					</div>
-				</div>
+				</carousel>
 			</section>
 			<section class="wrapper wrapper--shops">
 				<a href="#" class="shops__links"><img class="shops__img" :src="require('@/assets/img/label-shop/label-metro.svg')"></a>
@@ -85,3 +87,12 @@
 		</section>
 	</header>
 </template>
+
+<script>
+	import carousel from 'vue-owl-carousel'
+
+	export default {
+			components: { carousel },
+	}
+
+</script>
